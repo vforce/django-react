3b4bc930adce13a267d2b1231f4d82f703bead46
Dang Nguyen Anh Khoa <kdang@axon.com>
2023-01-28 07:26:04 +0700

    change field name, link the view

--- api/models.py
+++ api/models.py
@@ -14,6 +14,6 @@ def generate_unique_code(length: int = 8) -> str:
 class Room(models.Model):
     code = models.CharField(max_length=32, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
-    guess_can_pause = models.BooleanField(null=False, default=False)
+    guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
