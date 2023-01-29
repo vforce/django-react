47791451327e23797b217c7e5c9045deae4c20a9
Dang Nguyen Anh Khoa <kdang@axon.com>
2023-01-29 14:13:35 +0700

    everything is working

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Testing React Code</h1>;
+    }
+}
+
+const appDiv = document.getElementById("app");
+ReactDOM.render(<App/>, appDiv);
